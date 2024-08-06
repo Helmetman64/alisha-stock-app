cc47e74cf13039e5ed4ca82d21593b5d7eb34e09
DanielD26 <103547416@student.swin.edu.au>
2024-08-06 13:04:50 +1000

    Fixed the sell modal

--- src/components/Modals/ConfirmSellModal.js
+++ src/components/Modals/ConfirmSellModal.js
@@ -1,31 +1,94 @@
 import React from "react";
-import { Modal, Button } from "react-bootstrap";
+import { Modal, Button, Table } from "react-bootstrap";
 
-const ConfirmDeleteModal = ({
+const ConfirmSellModal = ({
   show,
   handleClose,
-  handleDeleteItem,
-  selectedItem,
+  items,
+  total,
+  deleteItem,
+  incrementQuantity,
+  decrementQuantity,
+  handleQuantityChange,
 }) => {
+  if (items.length === 0) {
+    handleClose();
+    return null;
+  }
+
   return (
-    <Modal show={show} onHide={handleClose} centered>
+    <Modal show={show} onHide={handleClose} centered size="lg">
       <Modal.Header closeButton>
-        <Modal.Title>Confirm Deletion</Modal.Title>
+        <Modal.Title>Sell Items</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        Are you sure you want to delete{" "}
-        {selectedItem ? selectedItem.itemName : ""}?
+        {items.length === 0 ? (
+          <p>No items to display.</p>
+        ) : (
+          <Table>
+            <thead>
+              <tr>
+                <th>Item</th>
+                <th>Unit Price</th>
+                <th>Quantity</th>
+                <th>Price</th>
+                <th />
+              </tr>
+            </thead>
+            <tbody>
+              {items.map((item) => (
+                <tr key={item.itemID}>
+                  <td>{item.itemName}</td>
+                  <td>${item.itemPrice.toFixed(2)}</td>
+                  <td>
+                    <div className="stock-quantity">
+                      <Button
+                        className="qty-btn"
+                        onClick={() => decrementQuantity(item.itemID)}
+                      >
+                        -
+                      </Button>
+                      <input
+                        type="number"
+                        value={item.count}
+                        className="stock-qty"
+                        onChange={(e) =>
+                          handleQuantityChange(item.itemID, e.target.value)
+                        }
+                        min="0"
+                      />
+                      <Button
+                        className="qty-btn"
+                        onClick={() => incrementQuantity(item.itemID)}
+                      >
+                        +
+                      </Button>
+                    </div>
+                  </td>
+                  <td>${(item.itemPrice * item.count).toFixed(2)}</td>
+                  <td>
+                    <i
+                      className="bi bi-trash clickable-icon"
+                      onClick={() => deleteItem(item.itemID)}
+                    ></i>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        )}
+        <div className="total-section">
+          <h4>Total: ${total}</h4>
+        </div>
       </Modal.Body>
       <Modal.Footer>
         <Button variant="secondary" onClick={handleClose}>
           Cancel
         </Button>
-        <Button variant="danger" onClick={handleDeleteItem}>
-          Confirm Delete
-        </Button>
+        <Button variant="success">Confirm Sale</Button>
       </Modal.Footer>
     </Modal>
   );
 };
 
-export default ConfirmDeleteModal;
+export default ConfirmSellModal;
